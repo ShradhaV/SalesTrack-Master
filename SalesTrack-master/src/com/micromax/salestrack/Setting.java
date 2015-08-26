e5bbe66a9473660c4563a9c2063fc5aa521804c1
ShradhaV <shradha.vijay@micromaxinfo.com>
2015-08-26 16:59:49 +0530

    Adding source code

---
+++ SalesTrack-master/src/com/micromax/salestrack/Setting.java
@@ -0,0 +1,13 @@
+
+package com.micromax.salestrack;
+
+public class Setting {
+
+	public static final boolean IS_TABLET = false;
+	public static final boolean IS_RUSSIA = true;
+	public static final boolean IS_LOLLIPOP = false;
+	public static final boolean IS_WINGTECH = false;
+	public static final boolean IS_TOPWEIZ = false;
+	public static final boolean IS_SINGLESIM = true;
+	
+}
